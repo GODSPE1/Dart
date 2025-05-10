f2d222ea578dfb311e6b506a291c3245ff34554d
GODSPE1 <godspy18@gmail.com>
2025-05-10 22:58:31 +0100

    followed dart documentation and comment standard

--- 0-hello.dart
+++ 0-hello.dart
@@ -1,9 +1,7 @@
-/**
- * main - Programs prints exactly
- *        'Dart is fun, powerful, and expressive.'
- * 
- * Return: Nothing
- */
+/// main - Programs prints exactly
+///        'Dart is fun, powerful, and expressive.'
+ 
+ // Return: Nothing
 
 void main() {
   print('Dart is fun, powerful, and expressive.');
