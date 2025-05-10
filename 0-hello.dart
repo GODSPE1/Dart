84d01c108890b67860999862648a72abd8fc8e03
GODSPE1 <godspy18@gmail.com>
2025-05-10 22:38:13 +0100

    print strings to the console

---
+++ 0-hello.dart
@@ -0,0 +1,10 @@
+/**
+ * main - Programs prints exactly
+ *        'Dart is fun, powerful, and expressive.'
+ * 
+ * Return: Nothing
+ */
+
+void main() {
+  print('Dart is fun, powerful, and expressive.');
+}
