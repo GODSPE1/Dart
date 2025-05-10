794d66fdf5890e7cf6776751f4fd8a7f27674148
GODSPE1 <godspy18@gmail.com>
2025-05-10 23:11:54 +0100

    program that demonstrates the use of variables

---
+++ 1-variables.dart
@@ -0,0 +1,19 @@
+/// Write a Dart program that demonstrates the use of:
+/// [String] [int] [double] [bool] [final] [const]
+/// 
+void main() {
+  String language = 'Dart';
+
+  int year = 2011;
+
+  const double version = 3.2;
+
+  final bool isAwsome = true;
+
+
+  print('Language: ${language}');
+  print('Year: ${year}');
+  print('Version: ${version}');
+  print('Is Dart awesome? ${isAwsome}');
+
+}
